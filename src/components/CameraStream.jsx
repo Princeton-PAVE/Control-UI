01fe7c154d3c177f5a471fe31aa73481e2e743ed
Micheal Huang <65090844+astoppop@users.noreply.github.com>
2026-02-11 17:28:51 -0500

    Move ip + port to .env

--- src/components/CameraStream.jsx
+++ src/components/CameraStream.jsx
@@ -1,9 +1,13 @@
+import { API_BASE } from "../config";
+
 function CameraStream() {
+  const videoApi = `${API_BASE}/api/video_feed`;
+
   return (
     <div>
       <h2>Live Camera Feed</h2>
       <img
-        src="http://10.49.223.131:8000/api/video_feed"
+        src={videoApi}
         alt="Camera Stream"
         style={{
           width: "640px",
