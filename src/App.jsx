ccb66fdd883b8731d9ff77e5f3f275a18e7e7b99
Micheal Huang <65090844+astoppop@users.noreply.github.com>
2026-02-16 18:13:15 -0500

    Fix map rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import io from "socket.io-client";
@@ -9,6 +10,7 @@ const socket = io(API_BASE);
 function App() {
   const [count, setCount] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
+  const position = [51.505, -0.09];
 
   useEffect(() => {
     socket.on("connected", (data) => {
@@ -30,7 +32,22 @@ function App() {
   return (
     <>
       <h1>Socket.IO Only App</h1>
-
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "500px", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
       <button
         onClick={() => {
           socket.emit("increase_count");
@@ -38,22 +55,8 @@ function App() {
       >
         count is {count}
       </button>
-
       <p>The current time is {new Date(currentTime * 1000).toLocaleString()}</p>
-
       <CameraStream socket={socket} />
-
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
     </>
   );
 }
